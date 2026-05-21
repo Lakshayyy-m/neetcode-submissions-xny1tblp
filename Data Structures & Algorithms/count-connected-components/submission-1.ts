d64ff925c92dff674cf43a0a70c901a13d2677f3
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-05-21 11:07:46 -0700

    Add: count-connected-components - submission-1

---
+++ Data Structures & Algorithms/count-connected-components/submission-1.ts
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @param {number[][]} edges
+     * @returns {number}
+     */
+    countComponents(n: number, edges: number[][]): number {
+        //given number of nodes and list of edges
+        // DFS should be done on each node, keeping tract of visited, only visit if not visited before, depicting a different graph
+        // Everytime you traverse, just increase the count
+        const adjacencyList = {};
+        // adding all the nodes
+        //adding all the edges
+        for (let [node1, node2] of edges) {
+            if (!adjacencyList[node1]) adjacencyList[node1] = [];
+            if (!adjacencyList[node2]) adjacencyList[node2] = [];
+            adjacencyList[node1].push(node2);
+            adjacencyList[node2].push(node1);
+        }
+        console.log(adjacencyList);
+        // graph is ready
+        const visited = new Set<number>();
+        let countOfComponents = 0;
+        const dfs = (node: number) => {
+            if (visited.has(node)) return;
+            visited.add(node);
+            for (let neighbor of adjacencyList[node]) {
+                dfs(neighbor);
+            }
+        };
+
+        for (let i = 0; i < n; i++) {
+            if (visited.has(i)) continue;
+            countOfComponents++;
+            dfs(i);
+        }
+
+        return countOfComponents;
+    }
+}
