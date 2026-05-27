3e13372eb6252a50fcc93042485eb5bf1e7b9bcd
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-05-27 07:36:22 -0700

    Add: climbing-stairs - submission-5

---
+++ Data Structures & Algorithms/climbing-stairs/submission-5.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number} n
+     * @return {number}
+     */
+    climbStairs(n: number): number {
+        if (n <= 1) return 1;
+        const memoize = {};
+
+        // Finding the number of distinct ways
+        // The decision tree will be divided in 2 paths always (1 step taken and 2 steps taken)
+        // decision tree works, good
+        // Now we need to figure out memoization
+
+        function climb(n: number): number {
+            if (memoize[n]) return memoize[n];
+            if (n <= 1) return 1;
+            memoize[n] = climb(n - 1) + climb(n - 2);
+            return memoize[n];
+        }
+
+        return climb(n - 1) + climb(n - 2);
+    }
+}
