c48b86a6b0ff02c96682893364617d23f9a1971f
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-05-31 14:55:45 -0700

    Add: house-robber-ii - submission-5

---
+++ Data Structures & Algorithms/house-robber-ii/submission-5.ts
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    rob(nums: number[]): number {
+        // Basic house robber first and then the optimization
+        // logic difference is that after the last index, we have the first one.
+        if (nums.length < 4) return Math.max(...nums)
+        const memo = Array.from({ length: nums.length }, () => Array(2).fill(null));
+
+        const dfs = (index: number, isFirstValue: number = 0) => {
+            if (memo[index][isFirstValue]) return memo[index][isFirstValue];
+            let valueToAdd = 0;
+            if (nums.length > index + 3) {
+                if (index + 3 === nums.length - 1 && isFirstValue) {
+                    //if last element and first value starter
+                    valueToAdd = nums[index + 2];
+                } else {
+                    valueToAdd = Math.max(
+                        dfs(index + 2, isFirstValue),
+                        dfs(index + 3, isFirstValue),
+                    );
+                }
+            } else if (nums.length > index + 2) {
+                if (!(index + 2 === nums.length - 1 && isFirstValue)) valueToAdd = nums[index + 2];
+            }
+            memo[index][isFirstValue] = nums[index] + valueToAdd;
+            return memo[index][isFirstValue];
+        };
+
+        return Math.max(dfs(0, 1), dfs(1));
+    }
+}
