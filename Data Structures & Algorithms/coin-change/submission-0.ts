2387e9c9333bf76ddcf11322c0cfe72814898725
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-06-07 15:09:31 -0700

    Add: coin-change - submission-0

---
+++ Data Structures & Algorithms/coin-change/submission-0.ts
@@ -0,0 +1,28 @@
+class Solution {
+    /**
+     * @param {number[]} coins
+     * @param {number} amount
+     * @return {number}
+     */
+    coinChange(coins: number[], amount: number): number {
+        if (amount === 0) return 0;
+        let coinsUsed = 0;
+        let amountLeft = amount;
+        let index = coins.length - 1;
+        while (amountLeft > 0 && index > -1) {
+            if (amountLeft >= coins[index]) {
+                // this is the biggest denomination I can subtract
+                amountLeft -= coins[index];
+                coinsUsed++;
+            } else {
+                index--;
+            }
+        }
+
+        if(amountLeft > 0) {
+            return -1
+        }
+
+        return coinsUsed
+    }
+}
