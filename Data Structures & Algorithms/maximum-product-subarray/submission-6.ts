0c77ba590116cf0ccbb638649ad14dc6177a4ddb
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-06-09 07:24:02 -0700

    Add: maximum-product-subarray - submission-6

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-6.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxProduct(nums: number[]): number {
+        let max = 1;
+        let min = 1;
+        let result = nums[0];
+
+        for (let i = 0; i < nums.length; i++) {
+            let temp = max * nums[i];
+            max = Math.max(nums[i], max * nums[i], min * nums[i]);
+            min = Math.max(nums[i], min * nums[i], temp);
+            result = Math.max(result, max)
+        }
+
+        return result;
+    }
+}
