747cb700566ec29902fd32376fe99bb317bba753
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-06-17 07:29:37 -0700

    Add: word-break - submission-3

---
+++ Data Structures & Algorithms/word-break/submission-3.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string[]} wordDict
+     * @return {boolean}
+     */
+    wordBreak(s: string, wordDict: string[]): boolean {
+        const dp = new Array(s.length + 1).fill(false);
+        dp[s.length] = true;
+
+        for (let i = s.length - 1; i >= 0; i--) {
+            for (let word of wordDict) {
+                if (word.length <= s.length - i && s.substring(i, i + word.length) === word) {
+                    dp[i] = dp[i + word.length];
+                }
+                if (dp[i]) break;
+            }
+        }
+        return dp[0];
+    }
+}
