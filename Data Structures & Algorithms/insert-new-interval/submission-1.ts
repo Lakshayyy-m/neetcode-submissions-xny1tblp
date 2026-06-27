452a0622ee99b3fd5fc9a2e6603e72e4faa0e64e
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-06-27 14:07:51 -0700

    Add: insert-new-interval - submission-1

---
+++ Data Structures & Algorithms/insert-new-interval/submission-1.ts
@@ -0,0 +1,25 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @param {number[]} newInterval
+     * @return {number[][]}
+     */
+    insert(intervals: number[][], newInterval: number[]): number[][] {
+        const result = [];
+
+        for (let i = 0; i < intervals.length; i++) {
+            if (newInterval[1] < intervals[i][0]) {
+                result.push(newInterval, ...intervals.slice(i));
+                return result;
+            } else if(newInterval[0] > intervals[i][1]){
+                result.push(intervals[i])
+            } else {
+                newInterval = [Math.min(newInterval[0], intervals[i][0]), Math.max(newInterval[1], intervals[i][1])]
+            }
+        }
+
+        result.push(newInterval)
+
+        return result;
+    }
+}
