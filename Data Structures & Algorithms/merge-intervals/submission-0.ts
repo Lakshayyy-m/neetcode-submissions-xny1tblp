1f3f8abe74a28dcd1e8cabd4fa3f3072a47235b9
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-06-29 19:59:24 -0700

    Add: merge-intervals - submission-0

---
+++ Data Structures & Algorithms/merge-intervals/submission-0.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[][]} intervals
+     * @return {number[][]}
+     */
+    merge(intervals: number[][]): number[][] {
+        const result = [];
+
+        let currentStart: number = intervals[0][0];
+        let currentEnd: number = intervals[0][1];
+        for (let i = 1; i < intervals.length; i++) {
+            let [startTime, endtime] = intervals[i];
+            if (currentEnd >= startTime) {
+                // Overlapping intervals - keep same start time, and take the next's end time
+                currentEnd = endtime;
+            } else {
+                result.push([currentStart, currentEnd]);
+                currentStart = startTime;
+                currentEnd = endtime;
+            }
+        }
+
+        result.push([currentStart, currentEnd]);
+
+        return result;
+    }
+}
