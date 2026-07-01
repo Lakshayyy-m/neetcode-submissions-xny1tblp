b91ab41910f719d7941acc04d31e29b933b9e688
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-07-01 11:56:58 -0700

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition of Interval:
+ * class Interval  {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals: Interval[]): boolean {
+        intervals.sort((a, b) => a.start - b.start);
+
+        for (let i = 1; i < intervals.length; i++) {
+            if (intervals[i - 1].end > intervals[i].start) {
+                // previus end is bigger than current start -> conflict
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
