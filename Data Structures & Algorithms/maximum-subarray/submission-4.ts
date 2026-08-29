7106948fdc4a26c89156bd2dc39689ae462c7fd3
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-08-29 13:36:34 -0700

    Add: maximum-subarray - submission-4

---
+++ Data Structures & Algorithms/maximum-subarray/submission-4.ts
@@ -0,0 +1,26 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    maxSubArray(nums: number[]): number {
+        let l = 0;
+        let r = 0;
+        let max = nums[0];
+        let lastSum = nums[0];
+        while (r < nums.length - 1) {
+            r++;
+            if (lastSum + nums[r] <= 0 && r < nums.length - 1) {
+                l = r + 1;
+                r = r + 1;
+                console.log(l);
+                lastSum = nums[l];
+            } else {
+                lastSum += nums[r];
+            }
+
+            max = Math.max(max, lastSum);
+        }
+        return max;
+    }
+}
