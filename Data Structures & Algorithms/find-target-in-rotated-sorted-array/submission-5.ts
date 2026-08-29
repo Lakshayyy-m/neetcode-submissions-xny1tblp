5c6a7b1c8f62a0dc14ec4c6de9e5c2c7396c5a73
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-08-28 20:15:58 -0700

    Add: find-target-in-rotated-sorted-array - submission-5

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-5.ts
@@ -0,0 +1,38 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        let l = 0;
+        let r = nums.length - 1;
+
+        while (l <= r) {
+            let mid = Math.floor((l + r) / 2);
+            if (target === nums[mid]) return mid;
+
+            if (nums[l] < nums[mid]) {
+                // mid is in the left sorted portion
+                if (target > nums[mid] || target < nums[l]) {
+                    //discard left
+                    l = mid + 1;
+                } else {
+                    //discard right
+                    r = mid - 1;
+                }
+            } else {
+                // mid is in the right sorted ported
+                if (target > nums[mid] && target < nums[r]) {
+                    // discard right
+                    l = mid + 1;
+                } else {
+                    // discard left
+                    r = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
