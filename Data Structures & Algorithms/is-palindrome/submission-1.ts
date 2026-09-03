5a60e2b130d0f781287595b57d4650a352c22be0
Lakshay Manchanda <lakshaymanchanda06@gmail.com>
2026-09-03 15:08:54 -0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.ts
@@ -0,0 +1,35 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {boolean}
+     */
+    isPalindrome(s: string): boolean {
+        //requirements
+        // Letters same no matter you traverse from back or front -> Done
+        // Case insensitive -> Done
+        // Non alphanumeric chars skipped -> Check regex
+
+        let l = 0;
+        let r = s.length - 1;
+        let regex = /^[a-z0-9]+$/;
+
+        while (l < r) {
+            while (l < r && !regex.test(s[l].toLowerCase())) {
+                console.log(s[l]);
+                l++;
+            }
+            while (r > l && !regex.test(s[r].toLowerCase())) {
+                console.log(s[r]);
+                r--;
+            }
+            if (s[l].toLowerCase() !== s[r].toLowerCase()) {
+                console.log(s[l], s[r]);
+                return false;
+            }
+            l++;
+            r--;
+        }
+
+        return true;
+    }
+}
